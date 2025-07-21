a1c9d23dcb3b05bf8ffbf0302e3cb5c301bb1cb3
yusera-ansari <yuserabanoansari@dwao.in>
2025-07-21 14:03:35 +0530

    Initial Commit

---
+++ Srumdinger/SrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SrumdingerApp.swift
+//  Srumdinger
+//
+//  Created by Abcom on 21/07/25.
+//
+
+import SwiftUI
+
+@main
+struct SrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SrumdingerTests/SrumdingerTests.swift
@@ -0,0 +1,17 @@
+//
+//  SrumdingerTests.swift
+//  SrumdingerTests
+//
+//  Created by Abcom on 21/07/25.
+//
+
+import Testing
+@testable import Srumdinger
+
+struct SrumdingerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
